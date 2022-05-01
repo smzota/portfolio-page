b88839e5c35b4f1eb0d260e625689843ab804773
smzota <smzota@gmail.com>
2022-05-01 10:46:55 +0200

    add animate effect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(()=>{
     $('.header').height($(window).height())
 })
+$(".navbar a").click(()=>{
+    $("body,html").animate({
+        scrollTop:$("#"+$(this).data('data-value')).offset().top
+    }, 1000)
+})
